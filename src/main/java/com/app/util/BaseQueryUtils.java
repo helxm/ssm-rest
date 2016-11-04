417b70f0bda9c5dfc09f7b34773051243f416dfc
hesh <764009577@qq.com>
2016-11-04 18:32:48 +0800

    ...

--- src/main/java/com/app/util/BaseQueryUtils.java
+++ src/main/java/com/app/util/BaseQueryUtils.java
@@ -1,7 +1,10 @@
 package com.app.util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.rainbowbus.bean.base.BaseBean;
 import com.rainbowbus.service.impl.api.BaseServiceImpl;
@@ -14,10 +17,57 @@ import com.rainbowbus.service.impl.api.BaseServiceImpl;
 public class BaseQueryUtils {
 	
 	private static BaseServiceImpl<BaseBean> service;
+	private static ThreadLocal<List<Map<String, Object>>> valueLocal = new ThreadLocal<>();
 	
 	private BaseQueryUtils() {
 	}
 	
+	/**
+	 * 当获取多个字段值时，初始化查询，把结果放在ThreadLocal中
+	 * 
+	 * 然后可通过getValues、getValue方法以此获取列值
+	 * 
+	 * [注意]：可做统计查询，统计字段返回Long
+	 * 
+	 * @param columns
+	 * @param table
+	 * @param where
+	 * @return 
+	 */
+	public static void initValues(String[] columns,String table,String where) {
+		valueLocal.set(getColumns(columns, table, where));
+	}
+	public static List<Map<String, Object>> getMaps() {
+		return valueLocal.get();
+	}
+	public static List<Object> getValues(String column) {
+		List<Map<String, Object>> values = valueLocal.get();
+		if(values != null && values.size() > 0){
+			List<Object> list = new ArrayList<>(); 
+			for(Map<String, Object> map : values){
+				if(map != null && map.get(column) != null && StringUtils.isNotBlank(map.get(column).toString())){
+					list.add(map.get(column));
+				}
+			}
+			return list;
+		}
+		return null;
+	}
+	public static Object getValue(String column) {
+		List<Object> values = getValues(column);
+		if(values != null && values.size() > 0){
+			return values.get(0);
+		}
+		return null;
+	}
+	/**
+	 * 列表查询，多条记录
+	 * 
+	 * @param columns
+	 * @param table
+	 * @param where
+	 * @return
+	 */
 	@SuppressWarnings("unchecked")
 	public static List<Map<String, Object>> getColumns(String[] columns,String table,String where){
 		service = (BaseServiceImpl<BaseBean>) SpringInit.getBean("baseServiceImpl");
@@ -29,6 +79,7 @@ public class BaseQueryUtils {
     	select = select.substring(1, select.length());
     	
 		bean.setSql("select " + select + " from " + table + " where " + where);
+		System.out.println("BaseQueryUtils 执行 sql：" + "select " + select + " from " + table + " where " + where);
 		List<Map<String, Object>> values =  service.findMap(bean);
 		if(values != null && values.size() > 0 && values.get(0) != null){
 			return values;
@@ -46,7 +97,29 @@ public class BaseQueryUtils {
 	public static Object getObject(String column,String table,String where){
 		List<Map<String, Object>> values = getColumns(new String[]{column}, table, where);
 		if(values != null && values.size() > 0 && values.get(0) != null){
-			return values.get(0).get(column);
+			/*column = column.replaceAll("`", "");
+			if(column.contains("as ")){
+				column = column.split("as ")[1];
+			}*/
+			Map<String, Object> map = values.get(0);
+			for(Object value : map.values()){
+				return value;
+			}
+		}
+		return null;
+	}
+	/**
+	 * 获取一条记录的多列
+	 * 
+	 * @param column
+	 * @param table
+	 * @param where
+	 * @return
+	 */
+	public static Map<String,Object> getMap(String[] columns,String table,String where){
+		List<Map<String, Object>> values = getColumns(columns, table, where);
+		if(values != null && values.size() > 0 ){
+			return values.get(0);
 		}
 		return null;
 	}
@@ -76,8 +149,8 @@ public class BaseQueryUtils {
 	public static Integer getInt(String column,String table,String where){
 		Integer value = null;
 		try {
-			value = Integer.valueOf(getString(column, table, where));
-		} catch (NumberFormatException e) {
+			value = Integer.valueOf(getString(column, table, where).split("\\.")[0]);
+		} catch (Exception e) {
 		}
 		return value ;
 	}
@@ -92,8 +165,8 @@ public class BaseQueryUtils {
 	public static Long getLong(String column,String table,String where){
 		Long value = null;
 		try {
-			value = Long.valueOf(getString(column, table, where));
-		} catch (NumberFormatException e) {
+			value = Long.valueOf(getString(column, table, where).split("\\.")[0]);
+		} catch (Exception e) {
 		}
 		return value;
 	}
@@ -109,12 +182,27 @@ public class BaseQueryUtils {
 		Character value = null;
 		try {
 			value = getString(column, table, where).charAt(0);
-		} catch (IndexOutOfBoundsException e) {
+		} catch (Exception e) {
 		}
 		return value;
 	}
+	/**
+	 * update
+	 * 
+	 * @param column
+	 * @param table
+	 * @param where
+	 * @return
+	 */
+	public static int update(String sql){
+		service = (BaseServiceImpl<BaseBean>) SpringInit.getBean("baseServiceImpl");
+    	BaseBean bean = new BaseBean();
+    	bean.setSql(sql);
+		return service.updateBase(bean);
+	}
 	public static void main(String[] args) {
-		System.err.println((Long) (Object)"gfds" );//java.lang.String cannot be cast to java.lang.Long
+		//System.err.println((Long) (Object)"gfds" );//java.lang.String cannot be cast to java.lang.Long
+		System.err.println(Integer.valueOf("15".split("\\.")[0]));
 	}
 	
 }
